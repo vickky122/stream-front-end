b1a1111fe25788afcf368b5a123d964d3a5b2e4b
Vikrant Yadav <vikrantmani45@gmail.com>
2024-09-13 15:20:51 +0530

    API to get all the streamed video and see any particular video with the videoId

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,25 @@ import { Toaster } from "react-hot-toast";
 function App() {
   const [count, setCount] = useState(0);
 
+  const [videoId,setvideoId]=useState('adca8a03-b973-4b89-af93-a2cbb7ba7409')
+
   return (
     <>
       <Toaster />
+      <div>
       <div className="flex flex-col items-center space-y-9 justify-center py-9">
         <h1 className="text-2xl font-bold text-gray-700 dark:text-gray-100">
           Video Streaming App
         </h1>
+
+        <div>
+
+            <h1 className="text-red-900">Playing video</h1>
+            <video src='http://localhost:8090/api/v1/videos/stream/${videoId}' controls></video>
+
+        </div>
         <VideoUpload />
+        </div>
       </div>
     </>
   );
